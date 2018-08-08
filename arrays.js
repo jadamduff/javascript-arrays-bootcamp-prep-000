9d94f05c1f6e3287ca308eb8904c2505cb6ac855
Adam Duff <j.adam.duff@gmail.com>
2018-08-08 04:05:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-var addElementToBeginningOfArray = function() {
-  
+var addElementToBeginningOfArray = function(array, element) {
+  return [element, ...array];
 }
 
-var destructivelyAddElementToBeginningOfArray = function() {
-  
+var destructivelyAddElementToBeginningOfArray = function(array, element) {
+  return array.unshift(element);
 }
