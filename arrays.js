757798724c4fb2e1b30e84a2b8f6bbfc362f993f
Adam Duff <j.adam.duff@gmail.com>
2018-08-08 04:01:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 var addElementToBeginningOfArray = function() {
   
 }
+
+var destructivelyAddElementToBeginningOfArray = function() {
+  
+}
