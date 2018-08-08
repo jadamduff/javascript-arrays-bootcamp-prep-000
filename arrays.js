dadf0931282a01d087a656f8a189b112e0b8ed5d
Adam Duff <j.adam.duff@gmail.com>
2018-08-08 04:09:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,6 @@ var addElementToBeginningOfArray = function(array, element) {
 }
 
 var destructivelyAddElementToBeginningOfArray = function(array, element) {
-  array = [element, ...array];
+  array.unshift(element)
   return array;
 }
